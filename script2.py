13c7c7a2c5076ce68312d3de9aca94c50e7393cf
magnus850 <magnus.mora.askvig@gmail.com>
2026-01-20 17:47:10 +0100

    gjorde systemet mer brukervennlig ved å legge til flere alternativer.

--- script2.py
+++ script2.py
@@ -1,6 +1,11 @@
 import mariadb
 import bcrypt
 import pwinput
+import cutie
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 max_passord_lengde = 24
 min_passord_lengde = 6
@@ -8,14 +13,17 @@ max_brukernavn_lengde = 18
 min_brukernavn_lengde = 6
 
 conn = mariadb.connect(
-    user='magnus',
-    password='Smogholst1',
-    host = 'localhost',
-    database = 'brukersystem',
+    user = os.getenv('bruker'),
+    password= os.getenv('passord'),
+    host = os.getenv('host'),
+    database = os.getenv('navn'),
     unix_socket = '/opt/homebrew/var/mysql/mysql.sock'
     )
 
 cur = conn.cursor()
+
+ja_nei = ['ja',
+          'nei']
 
 def lag_bruker():
     while True:
@@ -38,11 +46,10 @@ def lag_bruker():
                 break
             print('Hold passorlengde innen 6-24 tegn')
             
-        passord_hashing(nytt_passord, nytt_brukernavn, None, None)
+        passord_hashing(nytt_passord, nytt_brukernavn, None, None, None)
     
     
 def logg_inn():
-    print("Logg inn")
     while True:
         brukernavn = input('Brukernavn(6-18): ')
         if min_brukernavn_lengde <= len(brukernavn) <= max_brukernavn_lengde:
@@ -54,26 +61,39 @@ def logg_inn():
         (brukernavn,))
     resultat = cur.fetchone()
     if resultat == None:
-        print('Feil brukernavn')
+        print('Finnes ikke bruker med det brukernavnet')
+        print('Prøv et annet brukernavn?')
+        ja = ja_nei[cutie.select(ja_nei)]
+        if ja == 'ja':
+            logg_inn()
+        if ja == 'nei':
+            lag_bruker_logg_inn()
         return
     if resultat:
-        while True:
-            passord = pwinput.pwinput(prompt='Passord(6-24): ', mask = '*')
-            if min_passord_lengde <= len(passord) <= max_passord_lengde:
-                break
-            print('Hold passorlengde innen 6-24 tegn')
-        liste = list(resultat)
-        passord_hashing(passord, None, liste[0], liste[1])
+        logg_inn_passord(resultat)
+        
+def logg_inn_passord(resultat):
+    while True:
+        passord = pwinput.pwinput(prompt='Passord(6-24): ', mask = '*')
+        if min_passord_lengde <= len(passord) <= max_passord_lengde:
+            break
+        print('Hold passorlengde innen 6-24 tegn')
+    liste = list(resultat)
+    passord_hashing(passord, None, liste[0], liste[1], resultat)
         
         
 def registrer_bruker(passord, brukernavn):
     cur.execute('insert into brukere (bruker, passord) values (%s, %s)',
         (brukernavn, passord))
     if cur.fetchone:
-        print(f'Du har lagd en bruker ved navn {brukernavn}')
+        print(f'Du er nå logget inn som {brukernavn}')
+        cur.execute('select id from brukere where bruker = %s', (brukernavn,))
+        id = list(cur.fetchone(
+        ))
+        endre_passord_eller_logg_ut(id)
 
 
-def passord_hashing(input_passord, input_brukernavn, id, db_passord):
+def passord_hashing(input_passord, input_brukernavn, id, db_passord, resultat):
     byte_passord = input_passord.encode('utf-8')
     salt = bcrypt.gensalt()
     hasha_passord = bcrypt.hashpw(byte_passord, salt)
@@ -83,54 +103,69 @@ def passord_hashing(input_passord, input_brukernavn, id, db_passord):
     if input_brukernavn == None and db_passord == None:
         registrer_nytt_passord(hasha_passord_string, id)
         return
-    
+
     #sjekke passord
     if input_brukernavn == None:
         input_byte = input_passord.encode('utf-8')
         passord_byte = db_passord.encode('utf-8')
         if bcrypt.checkpw(input_byte, passord_byte):
-            print('Du er logget inn')
-            endre_passord(id)
+            print(f'Du er logget inn')
+            endre_passord_eller_logg_ut(id)
             return
         else: 
-            print('Feil passord') 
-            return
+            print('Feil passord, prøv på nytt?')
+            ja = ja_nei[cutie.select(ja_nei)]
+            if ja == 'ja':
+                logg_inn_passord(resultat)
+            if ja == 'nei':
+                lag_bruker_logg_inn()
+
     registrer_bruker(hasha_passord_string, input_brukernavn)
     
-    
-def endre_passord(id):
-    endre_passord = input('Vil du endre passord? ja/nei: ')
-    if endre_passord == 'ja':
+muligheter =[
+    'Endre passord',
+    'Logg ut'
+]
+
+def endre_passord_eller_logg_ut(id):
+    mul = muligheter[cutie.select(muligheter)]
+    if mul == 'Logg ut':
+        print('Du er logget ut')
+        lag_bruker_logg_inn()
+        return
+    if mul == 'Endre passord':
         while True:
             nytt_passord = pwinput.pwinput(prompt='Passord(6-24): ', mask = '*')
             if min_passord_lengde <= len(nytt_passord) <= max_passord_lengde:
                 break
             print('Hold passorlengde innen 6-24 tegn')
-        passord_hashing(nytt_passord, None, id, None)
+        passord_hashing(nytt_passord, None, id, None, None)
         return
-    else: return
-    
-    
+        
+        
 def registrer_nytt_passord(passord, id):
     cur.execute(
     'UPDATE brukere SET passord = %s WHERE id = %s',
     (passord, id))
     if cur.fetchone:
         print('Du har endret passord')
+        endre_passord_eller_logg_ut(id)
     
-    
-def innlogging_eller_nybruker():
-    innlogging = input('Vil du logge inn? ja/nei: ')
-    if innlogging == 'ja':
-        logg_inn()
-    if innlogging == 'nei':
-        nybruker = input('Vil du lage en ny bruker? ja/nei: ')
-        if nybruker == 'ja':
-            print('Lag bruker')
-            lag_bruker()
-            
-            
-innlogging_eller_nybruker()
+print('Velkommen til Kurdistans offisielle brukerautentiseringsystem!')
+valg = [
+    'Lag bruker',
+    'Logg inn'
+]
 
+def lag_bruker_logg_inn():
+    valgt_valg = valg[cutie.select(valg)]
+    if valgt_valg == 'Lag bruker':
+        lag_bruker() 
+    if valgt_valg == 'Logg inn':
+        print('Logg inn')
+        logg_inn(
+                )
+
+lag_bruker_logg_inn()
 conn.commit()
 conn.close()
